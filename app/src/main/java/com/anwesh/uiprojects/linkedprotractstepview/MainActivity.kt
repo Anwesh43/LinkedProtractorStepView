62ff137a39b11ecffb88009edf3f278c9124e259
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 10:53:38 +0530

    adding View ew in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedprotractstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedprotractstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedprotractstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.protractstepview.ProtractStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ProtractStepView.create(this)
     }
 }
