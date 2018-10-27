91a530204af4281bc4590b3044a065c270b0aeb1
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 10:00:38 +0530

    adding ProtractStepView class

--- protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
+++ protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
@@ -38,3 +38,21 @@ fun Canvas.drawPSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ProtractStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
