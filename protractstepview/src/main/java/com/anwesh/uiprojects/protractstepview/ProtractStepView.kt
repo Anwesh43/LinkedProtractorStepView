af411493650fc1711a80b5cc870cdb743f763171
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 10:18:37 +0530

    adding PSNode class

--- protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
+++ protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
@@ -103,4 +103,49 @@ class ProtractStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PSNode(var i : Int, val state : State = State()) {
+
+        private var next : PSNode? = null
+
+        private var prev : PSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = PSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PSNode {
+            var curr : PSNode? = this.prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
