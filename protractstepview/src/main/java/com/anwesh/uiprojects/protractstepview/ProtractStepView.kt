227d3a474c6ef5825588fd6cb55d76b900ff0341
Anwesh43 <anweshthecool0@gmail.com>
2018-10-27 09:45:14 +0530

    importing libraries and defining nodes, color

--- protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
+++ protractstepview/src/main/java/com/anwesh/uiprojects/protractstepview/ProtractStepView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.protractstepview
 /**
  * Created by anweshmishra on 27/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val color : Int = Color.parseColor("#4CAF50")
